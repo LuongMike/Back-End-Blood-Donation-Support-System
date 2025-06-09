f50ee82c700f1946aada0279d6cb753d1f5632e3
binthbin <minhlongwa3@gmail.com>
2025-06-09 19:34:54 +0700

    create AppointmentRequestDTO for appointment registration

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/AppointmentRequestDTO.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/AppointmentRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.backend_blood_donation_system.dto;
 
 public class AppointmentRequestDTO {
+
     private Integer userId;
     private Integer centerId;
     private String scheduledDate;
