15fba1a7e82dee97395e47e819cbf6c90008df8b
binthbin <minhlongwa3@gmail.com>
2025-06-09 19:31:45 +0700

    add Appointment entity with relation to DonationCenter and User

---
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/Appointment.java
@@ -0,0 +1,30 @@
+package com.example.backend_blood_donation_system.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="Appointment")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer appointmentId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // Giả sử đã có entity User
+
+    @ManyToOne
+    @JoinColumn(name = "center_id", nullable = false)
+    private DonationCenter center;  // Giả sử đã có entity DonationCenter
+
+    private java.sql.Date scheduledDate;
+
+    @Column(length = 20)
+    private String status = "PENDING"; // default khi tạo mới
+
+    @Column(length = 50)
+    private String screeningResult;
+
+    @Lob
+    private String remarks;
+}
