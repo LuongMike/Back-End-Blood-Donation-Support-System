6b83df6ce32186d1edb596ae4957ba321b581f97
mlong05 <minhlongwa3@gmail.com>
2025-06-15 14:48:37 +0700

    Add service method to get user info by userId

---
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.backend_blood_donation_system.service;
+
+import com.example.backend_blood_donation_system.dto.UserInfoDTO;
+import com.example.backend_blood_donation_system.entity.User;
+import com.example.backend_blood_donation_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserInfoDTO getUserInfo(int userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+        return new UserInfoDTO(
+                user.getUsername(),
+                user.getGender(),
+                user.getPhoneNumber()
+        );
+    }
+
+}
