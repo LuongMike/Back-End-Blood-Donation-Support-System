3ed0b30971aece3d34e3708b8457f38dc62288e1
Luong <luongdz10vnvt@gmail.com>
2025-06-17 20:50:18 +0700

    feat(entity): add HealthQuestion entity for storing health-related questions

---
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/HealthQuestion.java
@@ -0,0 +1,37 @@
+package com.example.backend_blood_donation_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "HealthQuestion", schema = "dbo") // escape tên bảng HealthQuestion với dấu "
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HealthQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Integer id;
+
+    @Column(name = "question_text", nullable = false)
+    private String questionText;
+
+    @Column(name = "correct_answer")
+    private Boolean correctAnswer;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+}
+
