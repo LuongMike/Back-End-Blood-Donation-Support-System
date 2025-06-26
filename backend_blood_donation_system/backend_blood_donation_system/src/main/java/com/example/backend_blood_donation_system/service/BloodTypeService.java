64e5de9b8a2c07df9f3562e8ed405656ce2de9ab
mlong05 <minhlongwa3@gmail.com>
2025-06-26 08:26:56 +0700

    create get all blood type service

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/service/BloodTypeService.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/service/BloodTypeService.java
@@ -1,4 +1,21 @@
 package com.example.backend_blood_donation_system.service;
 
+import com.example.backend_blood_donation_system.dto.BloodTypeDTO;
+import com.example.backend_blood_donation_system.entity.BloodType;
+import com.example.backend_blood_donation_system.repository.BloodTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class BloodTypeService {
+    @Autowired
+    private BloodTypeRepository bloodTypeRepository;
+
+    public List<BloodTypeDTO> getAllBloodTypes() {
+        List<BloodType> bloodTypes = bloodTypeRepository.findAll();
+        return bloodTypes.stream()
+                .map(b -> new BloodTypeDTO(b.getBlood_type_id(), b.getType()))
+                .toList();
+    }
 }
