1fc7ffe8d513dd36f08fa111008f42061749fec2
mlong05 <minhlongwa3@gmail.com>
2025-06-26 08:29:59 +0700

    rename field bloodType to blood_type

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/UserProfileDTO.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/UserProfileDTO.java
@@ -1,8 +1,12 @@
 package com.example.backend_blood_donation_system.dto;
 
+import com.example.backend_blood_donation_system.entity.BloodType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserProfileDTO {
     private String fullName;
     private String email;
@@ -10,6 +14,9 @@ public class UserProfileDTO {
     private String address;
     private String gender;
     private Double weight;
-    private String bloodType;  // ví dụ: "A+", "O-", ...
+//    @EnumValue(name = "blood_group", enumClass = BloodType.class)
+//@ManyToOne
+//@JoinColumn(name = "blood_type_id")
+    private String blood_type;  // ví dụ: "A+", "O-", ...
     private String healthStatus;
 }
