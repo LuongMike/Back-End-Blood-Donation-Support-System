5a175f0913f3fe06429c9d30873a3bb492bf113d
mlong05 <minhlongwa3@gmail.com>
2025-06-26 08:29:09 +0700

    add get blood type API end point

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/UserController.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.backend_blood_donation_system.controller;
 
+import com.example.backend_blood_donation_system.dto.BloodTypeDTO;
 import com.example.backend_blood_donation_system.dto.UserInfoDTO;
 import com.example.backend_blood_donation_system.dto.UserProfileDTO;
 import com.example.backend_blood_donation_system.repository.UserRepository;
+import com.example.backend_blood_donation_system.service.BloodTypeService;
 import com.example.backend_blood_donation_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -14,13 +18,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private BloodTypeService bloodTypeService;
 
     @GetMapping("/{id}/profile")
     public ResponseEntity<UserProfileDTO> getProfile(@PathVariable int id) {
         return ResponseEntity.ok(userService.getProfile(id));
     }
 
-    @PutMapping("/{id}/profile")
+    @PatchMapping("/{id}/profile")
     public ResponseEntity<String> updateProfile(@PathVariable int id, @RequestBody UserProfileDTO dto) {
         userService.updateProfile(id, dto);
         return ResponseEntity.ok("Profile updated successfully");
@@ -29,6 +35,12 @@ public class UserController {
     public ResponseEntity<UserInfoDTO> getUserInfo(@PathVariable int id) {
         UserInfoDTO userInfo = userService.getUserInfo(id);
         return ResponseEntity.ok(userInfo);
+    }
+
+    @GetMapping("/blood_type")
+    public ResponseEntity<List<BloodTypeDTO>> getAllBloodTypes() {
+        List<BloodTypeDTO> bloodTypes = bloodTypeService.getAllBloodTypes();
+        return ResponseEntity.ok(bloodTypes);
     }
 
 }
