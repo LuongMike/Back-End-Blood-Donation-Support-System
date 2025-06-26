da600cee97ae2b09599f59528af0fc4c34350fae
mlong05 <minhlongwa3@gmail.com>
2025-06-26 08:30:39 +0700

    add findByUserId method

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/repository/UserProfileRepository.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/repository/UserProfileRepository.java
@@ -2,10 +2,13 @@ package com.example.backend_blood_donation_system.repository;
 
 import com.example.backend_blood_donation_system.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile,Integer> {
     Optional<UserProfile> findByUser_UserId(int userId);
+    @Query("SELECT p FROM UserProfile p WHERE p.user.userId = :userId")
+    Optional<UserProfile> findByUserId(Integer userId);
 
 }
