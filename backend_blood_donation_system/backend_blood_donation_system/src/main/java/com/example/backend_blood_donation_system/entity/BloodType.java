fd2274cf03b825731c20a878409136ff9d110fe1
mlong05 <minhlongwa3@gmail.com>
2025-06-26 08:24:40 +0700

    update id field to auto generate

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BloodType.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BloodType.java
@@ -1,20 +1,19 @@
 package com.example.backend_blood_donation_system.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "BloodType")
+@Table(name = "blood_type")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BloodType {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Hibernate dùng IDENTITY
     private Long blood_type_id;
 
     private String type;
