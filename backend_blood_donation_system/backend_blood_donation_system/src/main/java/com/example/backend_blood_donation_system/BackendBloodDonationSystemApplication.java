ec3871bb31fadbe67f9d3fd1fee7a0e4cf261aa0
baokhanh592004 <baokhanh21qb@gmail.com>
2025-07-03 11:08:42 +0700

    fix component type to Nvarchar so blood_inventory can show vietnamese

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/BackendBloodDonationSystemApplication.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/BackendBloodDonationSystemApplication.java
@@ -2,12 +2,15 @@ package com.example.backend_blood_donation_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan; // Import thêm
 
 @SpringBootApplication
+// THÊM DÒNG NÀY VÀO
+// Chỉ định rõ các gói chứa Entity cần quét
+@EntityScan(basePackages = {"com.example.backend_blood_donation_system.entity", "com.another.package.with.entities"}) 
 public class BackendBloodDonationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendBloodDonationSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBloodDonationSystemApplication.class, args);
+    }
 }
