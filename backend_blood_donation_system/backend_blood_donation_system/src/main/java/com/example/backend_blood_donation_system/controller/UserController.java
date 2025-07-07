8b40e4a137de4f4a363743913ce0634b3217cf96
mlong05 <minhlongwa3@gmail.com>
2025-07-07 20:47:40 +0700

    resolve conflict

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/UserController.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/UserController.java
@@ -1,27 +1,26 @@
 package com.example.backend_blood_donation_system.controller;
 
-<<<<<<< HEAD
+
 import com.example.backend_blood_donation_system.dto.NotificationResponse;
 import com.example.backend_blood_donation_system.dto.UserInfoDTO;
 import com.example.backend_blood_donation_system.entity.UserNotification;
 import com.example.backend_blood_donation_system.security.CustomUserDetails;
 import com.example.backend_blood_donation_system.service.NotificationService;
-=======
+
 import com.example.backend_blood_donation_system.dto.BloodTypeDTO;
 import com.example.backend_blood_donation_system.dto.UserInfoDTO;
 import com.example.backend_blood_donation_system.dto.UserProfileDTO;
 import com.example.backend_blood_donation_system.repository.UserRepository;
 import com.example.backend_blood_donation_system.service.BloodTypeService;
->>>>>>> Feature-View-User
+
 import com.example.backend_blood_donation_system.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-=======
->>>>>>> Feature-View-User
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,11 +32,11 @@ public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
-<<<<<<< HEAD
+
     private NotificationService notificationService;
-=======
+
     private BloodTypeService bloodTypeService;
->>>>>>> Feature-View-User
+
 
     @GetMapping("/{id}/profile")
     public ResponseEntity<UserProfileDTO> getProfile(@PathVariable int id) {
@@ -45,6 +44,7 @@ public class UserController {
     }
 
     @PatchMapping("/{id}/profile")
+
     public ResponseEntity<String> updateProfile(@PathVariable int id, @RequestBody UserProfileDTO dto) {
         userService.updateProfile(id, dto);
         return ResponseEntity.ok("Profile updated successfully");
@@ -54,7 +54,7 @@ public class UserController {
         UserInfoDTO userInfo = userService.getUserInfo(id);
         return ResponseEntity.ok(userInfo);
     }
-<<<<<<< HEAD
+
     @GetMapping("/notification")
     public ResponseEntity<?> getMyNotifications(@AuthenticationPrincipal CustomUserDetails user) {
         System.out.println("🔍 Logged in user ID = " + user.getId()); // in ra console
@@ -67,13 +67,13 @@ public class UserController {
                                                      @AuthenticationPrincipal CustomUserDetails userDetails) {
         notificationService.acceptDonationRequest(userDetails.getId(), notificationId);
         return ResponseEntity.ok("Bạn đã đồng ý hiến máu.");
-=======
+    }
 
     @GetMapping("/blood_type")
     public ResponseEntity<List<BloodTypeDTO>> getAllBloodTypes() {
         List<BloodTypeDTO> bloodTypes = bloodTypeService.getAllBloodTypes();
         return ResponseEntity.ok(bloodTypes);
->>>>>>> Feature-View-User
+
     }
 
 }
--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BloodType.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BloodType.java
@@ -1,6 +1,6 @@
 package com.example.backend_blood_donation_system.entity;
 
-<<<<<<< HEAD
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
