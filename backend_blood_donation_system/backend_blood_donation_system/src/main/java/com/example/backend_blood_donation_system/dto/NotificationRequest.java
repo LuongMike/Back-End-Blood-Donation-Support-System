44083d73137b42dfa22d57fbdc4779317bd113f7
mlong05 <minhlongwa3@gmail.com>
2025-07-07 20:18:39 +0700

    add notification type field

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/NotificationRequest.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/NotificationRequest.java
@@ -1,9 +1,11 @@
 package com.example.backend_blood_donation_system.dto;
 
+import com.example.backend_blood_donation_system.enums.NotificationType;
 import lombok.Data;
 
 @Data
 public class NotificationRequest {
     private String title;
     private String message;
+    private NotificationType type;
 }
