50d1969df959fc36f17400dbe254bdbfc6a2cb63
mlong05 <minhlongwa3@gmail.com>
2025-07-07 20:18:58 +0700

    add notification type and notification id field

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/NotificationResponse.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.example.backend_blood_donation_system.dto;
 
+import com.example.backend_blood_donation_system.enums.NotificationType;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,10 @@ import java.time.LocalDateTime;
 
 @Data
 public class NotificationResponse {
+    private Integer notificationId;
     private String title;
     private String message;
     private LocalDateTime createdAt;
     private boolean isRead;
+    private NotificationType type;
 }
