caaeb3d220e4c2818e93bfa58b579f4416f32dc4
baokhanh592004 <baokhanh21qb@gmail.com>
2025-07-07 19:28:48 +0700

    fetch(staff): api get user-profile by user id

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/StaffController.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/StaffController.java
@@ -3,20 +3,31 @@ package com.example.backend_blood_donation_system.controller;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.example.backend_blood_donation_system.dto.UserProfileRequestDTO;
-import com.example.backend_blood_donation_system.entity.UserProfile;
-import com.example.backend_blood_donation_system.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend_blood_donation_system.dto.DonationHistoryDTO;
 import com.example.backend_blood_donation_system.dto.RecordDonationRequestDTO;
 import com.example.backend_blood_donation_system.dto.ScreeningRequestDTO;
+import com.example.backend_blood_donation_system.dto.UserProfileRequestDTO;
 import com.example.backend_blood_donation_system.entity.Appointment;
 import com.example.backend_blood_donation_system.entity.BloodInventory;
+import com.example.backend_blood_donation_system.entity.UserProfile;
+import com.example.backend_blood_donation_system.service.AppointmentService;
+import com.example.backend_blood_donation_system.service.BloodInventoryService;
+import com.example.backend_blood_donation_system.service.DonationHistoryService;
+import com.example.backend_blood_donation_system.service.DonationRegistrationService;
+import com.example.backend_blood_donation_system.service.UserProfileService;
 
 import jakarta.validation.Valid;
 
@@ -136,5 +147,12 @@ public class StaffController {
     public ResponseEntity<?> deleteUserProfile(@PathVariable Integer userId) {
         userProfileService.deleteUserProfileByUserId(userId);
         return ResponseEntity.ok("Deleted user profile for user ID: " + userId);
+    }
+
+    // Lấy hồ sơ kiểm tra sức khỏe theo User ID
+    @GetMapping("/user-profile/{userId}")
+    public ResponseEntity<UserProfile> getUserProfileByUserId(@PathVariable Integer userId) {
+        UserProfile userProfile = userProfileService.findProfileByUserId(userId);
+        return ResponseEntity.ok(userProfile);
     }
 }
