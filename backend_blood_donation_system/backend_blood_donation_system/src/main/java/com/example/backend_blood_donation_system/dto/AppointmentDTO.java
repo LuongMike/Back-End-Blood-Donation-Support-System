c6b6038ca38f517381b2b43d59c0472fe4065189
baokhanh592004 <baokhanh21qb@gmail.com>
2025-07-10 11:00:46 +0700

    fix bug to get information from front end. change /appointments/by-date and /appointments

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/AppointmentDTO.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/AppointmentDTO.java
@@ -1,14 +1,16 @@
 package com.example.backend_blood_donation_system.dto;
 
-
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 public class AppointmentDTO {
     private Integer appointmentId;
-    private Integer centerId;
-    private Integer userId;
+    
+    // THAY ĐỔI Ở ĐÂY: Dùng đối tượng thay vì ID
+    private CenterSummaryDTO center;
+    private UserSummaryDTO user;
+
     private LocalDate scheduledDate;
     private String status;
     private String screeningResult;
