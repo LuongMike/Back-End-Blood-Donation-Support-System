5b2b32225449c7274cece4fc36effb9c22b7ac91
baokhanh592004 <baokhanh21qb@gmail.com>
2025-07-13 13:55:10 +0700

    change nvarchar 4000 to length = 4000 so SQL can make notification in khanh computer

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/Notification.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/Notification.java
@@ -1,11 +1,22 @@
 package com.example.backend_blood_donation_system.entity;
 
 
-import com.example.backend_blood_donation_system.enums.NotificationType;
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
+
+import com.example.backend_blood_donation_system.enums.NotificationType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -19,7 +30,7 @@ public class Notification {
 
     private String title;
 
-    @Column(columnDefinition = "NVARCHAR(MAX)")
+    @Column(length = 4000) 
     private String message;
 
     private LocalDateTime createdAt = LocalDateTime.now();
