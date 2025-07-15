ccb39c494f051f44d2553657e25127e4eae1c6c5
vominhluan17 <luanvmse182794@fpt.edu.vn>
2025-07-15 18:02:28 +0700

    feat: get visible posts by topic ID

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/ForumController.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/controller/ForumController.java
@@ -1,10 +1,12 @@
 package com.example.backend_blood_donation_system.controller;
 
+import com.example.backend_blood_donation_system.entity.ForumPost;
 import com.example.backend_blood_donation_system.entity.ForumTopic;
 import com.example.backend_blood_donation_system.service.ForumPostService;
 import com.example.backend_blood_donation_system.service.ForumTopicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,10 @@ public class ForumController {
     @GetMapping("/forum/topics")
     public List<ForumTopic> getAllTopics() {
         return topicService.getAllTopics();
+    }
+    @GetMapping("/forum/topics/{id}/posts")
+    public List<ForumPost> getVisiblePosts(@PathVariable Long id) {
+        return postService.getVisiblePosts(id);
     }
 
 }
