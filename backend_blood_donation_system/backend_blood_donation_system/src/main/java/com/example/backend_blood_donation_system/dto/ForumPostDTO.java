a54c7790f2f3bde1a6efb2ab27d9c9ea6a10371c
Luong <luongdz10vnvt@gmail.com>
2025-07-16 21:44:29 +0700

    refactor(forum): enhance DTOs and entity definitions

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/ForumPostDTO.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/dto/ForumPostDTO.java
@@ -1,13 +1,24 @@
 package com.example.backend_blood_donation_system.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ForumPostDTO {
     private Long id;
     private Long topicId;
     private String content;
     private Long authorId;
     private Boolean visible;
+    private LocalDateTime createdAt;
 }
 
