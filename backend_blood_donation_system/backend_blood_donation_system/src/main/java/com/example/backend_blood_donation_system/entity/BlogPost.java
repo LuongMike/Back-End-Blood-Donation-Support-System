8be2545f77f53b11b03d1e14d44e08a4df7bde27
baokhanh592004 <baokhanh21qb@gmail.com>
2025-07-16 09:03:18 +0700

    change to nvarchar

--- backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BlogPost.java
+++ backend_blood_donation_system/backend_blood_donation_system/src/main/java/com/example/backend_blood_donation_system/entity/BlogPost.java
@@ -1,12 +1,23 @@
 package com.example.backend_blood_donation_system.entity;
 
+import java.time.LocalDateTime;
+
 import com.example.backend_blood_donation_system.enums.PostType;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "blog_post")
@@ -22,8 +33,15 @@ public class BlogPost {
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
+    @Lob
+    @Column(columnDefinition = "NVARCHAR(MAX)")
     private String title;
+
+    @Lob
+    @Column(columnDefinition = "NVARCHAR(MAX)")
     private String content;
+
+    
     private String image;
 
     @Enumerated(EnumType.STRING)
